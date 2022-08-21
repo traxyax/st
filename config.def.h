22c0a2fd78a9ba4939541256249f53418004e836
Aboubacar TRAORE <traxyax@gmail.com>
2022-08-21 17:32:58 +0000

    Add st-scrollback-0.8.5.diff patch

--- config.def.h
+++ config.def.h
@@ -232,6 +232,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 	{ TERMMOD,              XK_S,           cyclefonts,     {}        },
 	{ MODKEY,               XK_o,           opencopied,     {.v = "xdg-open"} },
 	{ ACMPL_MOD,            XK_slash,       autocomplete,   { .i = ACMPL_WORD        } },
